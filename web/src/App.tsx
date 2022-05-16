c0d432488a88f463a9ec2875a04cb20da562536e
“monialodo” <monialodo@hotmail.com>
2022-05-16 14:33:44 -0300

    App Final

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ import styles from './App.module.scss'
 import { LoginBox } from './components/LoginBox'
 import { MessageList } from './components/MessageList'
 import { SendMessageForm } from './components/SendMessageForm'
-import { AuthContext } from './context /auth'
+import { AuthContext } from './context/auth'
 
 
 export function App() {
